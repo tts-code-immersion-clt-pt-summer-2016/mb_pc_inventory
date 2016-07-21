ed87da512f07eaa05ba400bb0b7a2b9345b48d84
PHILIP CANNING <philip.r.canning@gmail.com>
2016-07-21 16:42:25 -0400

    adds, adjusts, deletes, and exits if you put the word nothing

--- inventory.rb
+++ inventory.rb
@@ -2,18 +2,36 @@
 inventory = {"beer"=>20, "chips"=>50, "wine"=> 17, "veggies"=>32}
 
 puts inventory
+action = "something"
 
-puts "What would you like to do? ADJUST a current item, ADD a new item, or DELETE an item? (one word, please)"
-action = gets.chomp.upcase
+while action != "nothing"
+  puts "What would you like to do? ADJUST a current item, ADD a new item, DELETE an item, or NOTHING? (one word, please)"
+  action = gets.chomp.downcase
 
-puts "Great, which item would you like to #{action}?"
-item = gets.chomp.downcase
-
-if action == "ADD"
-  puts "How many #{item}s would you like to #{action}"
-  quantity = gets.chomp.to_i
-  inventory[item] = quantity
-  puts inventory
-else
-  puts "goodbye"
+  if action == "add"
+    puts "Great, which item would you like to #{action}?"
+    item = gets.chomp.downcase
+    puts "How many #{item}s would you like to #{action}"
+    quantity = gets.chomp.to_i
+    inventory[item] = quantity
+    puts inventory
+    sleep(1)
+  elsif action == "delete"
+    puts "Great, which item would you like to #{action}?"
+    item = gets.chomp.downcase
+    inventory.delete(item)
+    puts inventory
+    puts "#{item} has been #{action}d"
+    sleep(1)
+  elsif action == "adjust"
+    puts "Great, which item would you like to #{action}?"
+    item = gets.chomp.downcase
+    puts "By how much would you like to #{action} #{item}?"
+    adjustment = gets.chomp.to_i
+    inventory[item] = inventory[item] + adjustment
+    puts inventory
+    sleep(1)
+  else
+    puts "goodbye"
+  end
 end
